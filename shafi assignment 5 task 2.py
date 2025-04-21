39b66a40d271f22e21226c3d28faa002a97cc900
SHAIK MAHAMMED SHAFI <SMSSHAFI89@GMAIL.COM>
2025-04-21 15:03:37 +0530

    Update shafi assignment 5 task 2.py

--- shafi assignment 5 task 2.py
+++ shafi assignment 5 task 2.py
@@ -3,5 +3,5 @@ first_five_elements = numbers[:5]
 reverse_five_elements = first_five_elements.copy()
 reverse_five_elements.reverse()
 print('Original list:',numbers)
-print('Extracted firt five elements:',first_five_elements)
-print('Reversed extracted elemts:',reverse_five_elements)
+print('Extracted first five elements:',first_five_elements)
+print('Reversed extracted elements:',reverse_five_elements)
