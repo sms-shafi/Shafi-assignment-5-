f9c970ea590e0a79970d6fe24ae77f16efda79e7
SHAIK MAHAMMED SHAFI <SMSSHAFI89@GMAIL.COM>
2025-04-21 15:00:50 +0530

    Add files via upload

---
+++ shafi assignment 5 task 2.py
@@ -0,0 +1,7 @@
+numbers = list(range(1,11))
+first_five_elements = numbers[:5]
+reverse_five_elements = first_five_elements.copy()
+reverse_five_elements.reverse()
+print('Original list:',numbers)
+print('Extracted firt five elements:',first_five_elements)
+print('Reversed extracted elemts:',reverse_five_elements)
